8a4a16ced57b59fc6418857c530e8c3ed33986cd
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2021-04-26 18:03:11 +0200

    NEW: Support for QuickTime Movie MOV Format

---
+++ __init__.py
@@ -0,0 +1,46 @@
+#ffmpeg vim: ft=python fileencoding=utf-8 sw=4 et sts=4
+
+from typing import Any, BinaryIO
+
+from PyQt5.QtGui import QPixmap, QImageReader
+from PyQt5.QtCore import QProcess
+
+from vimiv import api
+from vimiv.utils import log
+
+_logger = log.module_logger(__name__)
+
+
+def test_mov(header: bytes, _f: BinaryIO) -> bool:
+    return header[4:8] == b"ftyp" and header[8:10] == b"qt"
+
+
+def load_frame(path) -> QPixmap:
+    """Extract the first frame from the video and initialize QPixmap"""
+
+    process = QProcess()
+    process.start(f"ffmpeg -loglevel quiet -i {path} -vframes 1 -f singlejpeg pipe:1")
+    process.waitForFinished()
+
+    if process.exitStatus() != QProcess.NormalExit or process.exitCode() != 0:
+        stderr = process.readAllStandardError()
+        raise ValueError(f"Error calling ffmpeg: '{stderr.data().decode()}'")
+
+    handler = QImageReader(process, "jpeg".encode())
+    handler.setAutoTransform(True)
+
+    process.closeWriteChannel()
+    process.terminate()
+
+    # Extract QImage from QImageReader and convert to QPixmap
+    pixmap = QPixmap()
+    pixmap.convertFromImage(handler.read())
+
+    return pixmap
+
+
+def init(info: str, *_args: Any, **_kwargs: Any) -> None:
+    """Setup RawPrev plugin by adding the raw handler"""
+    api.add_external_format("mov", test_mov, load_frame)
+
+    _logger.debug("Initialized RawPrev")
